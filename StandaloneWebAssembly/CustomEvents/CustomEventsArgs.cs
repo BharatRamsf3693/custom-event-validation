858e4e4d978e70cfd1ffa4446be894ed66eefbcb
BharatRamsf3693 <bharatram.h@syncfusion.com>
2026-09-16 17:48:18 +0530

    Add custom event validation samples and global.json

---
+++ StandaloneWebAssembly/CustomEvents/CustomEventsArgs.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StandaloneWebAssembly.CustomEvents
+{
+    public class ScrollTopEventArgs : EventArgs
+    {
+        public string? Message { get; set; }
+    }
+
+    [EventHandler("onscrolltop", typeof(ScrollTopEventArgs),
+    enableStopPropagation: true, enablePreventDefault: true)]
+
+    [EventHandler("oncustomscrolltop", typeof(ScrollTopEventArgs),
+    enableStopPropagation: true, enablePreventDefault: true)]
+    public static class EventHandlers
+    {
+    }
+}
