7ecd904aa735839064ae7b455cb1027c6d5919c2
BharatRamsf3693 <bharatram.h@syncfusion.com>
2026-09-18 18:04:44 +0530

    Update the samples and evidence for the Interactive Server built-in scroll event changes

---
+++ .NET11/InteractiveServer/Components/CustomEvents/CustomEventsArgs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace InteractiveServer.Components.CustomEvents
+{
+    public class CustomScrollEventArgs : EventArgs
+    {
+        public string? Detail { get; set; }
+    }
+
+    public class ScrollTopEventArgs : EventArgs
+    {
+        public string? Message { get; set; }
+    }
+
+    [EventHandler("onscrolltop", typeof(ScrollTopEventArgs),
+    enableStopPropagation: true, enablePreventDefault: true)]
+
+    [EventHandler("oncustomscrolltop", typeof(ScrollTopEventArgs),
+    enableStopPropagation: true, enablePreventDefault: true)]
+
+    [EventHandler("oncustomscroll", typeof(CustomScrollEventArgs),
+    enableStopPropagation: true, enablePreventDefault: true)]
+    public static class EventHandlers
+    {
+    }
+}
